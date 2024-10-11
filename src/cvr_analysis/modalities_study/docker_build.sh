f594b58eca53f025a9fcaa1931ab2f71859258ee
gurraburra <gustav.magnusson@liu.se>
2024-10-11 10:14:48 +0200

    change + for _ in docker tag name

--- src/cvr_analysis/modalities_study/docker_build.sh
+++ src/cvr_analysis/modalities_study/docker_build.sh
@@ -1,5 +1,5 @@
 NAME="cvr-analysis-modalities"
-VERSION=$(python -c "from cvr_analysis._version import __version__; print(__version__)")
+VERSION=$(python -c "from cvr_analysis._version import __version__; print(__version__.replace('+','_'))")
 
 caffeinate -i docker buildx build \
 	--platform linux/amd64,linux/arm64 \
