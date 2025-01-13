fa4ec86178f364f71ba10f9bbb14f7663df22911
Gustav Magnusson <bogustavmagnusson@gmail.com>
2025-01-13 11:35:22 +0100

    Added transform

--- src/cvr_analysis/legacy/plotting.py
+++ src/cvr_analysis/legacy/plotting.py
@@ -5,9 +5,11 @@ from nilearn import image
 import numpy as np
 import sys
 import json
+from scipy.ndimage import affine_transform
+from itertools import product
 
 class IMGShow:
-    def __init__(self, img, voxel_mask, settings, *plot_data):
+    def __init__(self, img, voxel_mask, settings, data_transform, *plot_data):
         # initate figure
         self.fig, self.axes = plt.subplots(2,2, figsize=(7,7))
         self.fig.tight_layout()
@@ -21,6 +23,7 @@ class IMGShow:
         self.img = img
         self.voxel_mask = voxel_mask
         self.settings = settings
+        self.data_transform = data_transform
         self.plot_data = plot_data
         # connect mpl
         self.fig.canvas.mpl_connect("button_press_event", self.buttonClick)
@@ -57,6 +60,10 @@ class IMGShow:
             self.coronal_ax.imshow(self.voxel_mask[:,self.pos[1],:].T, cmap = 'Greys', aspect = self.settings["aspect"], origin = self.settings["origin"], interpolation = 'nearest')
             self.axial_ax.imshow(self.voxel_mask[:,:,self.pos[2]].T, cmap = 'Greys', aspect = self.settings["aspect"], origin = self.settings["origin"], interpolation = 'nearest')
         # plot ax
+        pos = self.pos.copy()
+        pos.append(1)
+        pos = np.array(pos)[:,None]
+        pos = np.round(np.linalg.matmul(self.data_transform, pos), 0).astype(int)[:3,0]
         for data, label in self.plot_data:
             if isinstance(data, tuple):
                 times, data = data
@@ -69,17 +76,17 @@ class IMGShow:
                     self.plot_ax.plot(times, data, label = label)
             elif data.ndim == 3:
                 if times is None:
-                    self.plot_ax.axvline(data[self.pos[0], self.pos[1], self.pos[2]], label = label)
+                    self.plot_ax.axvline(data[pos[0], pos[1], pos[2]], label = label)
                 else:
                     raise ValueError("Time cannot be defined fore 3D data")
             elif data.ndim == 4:
                 if times is None:
-                    self.plot_ax.plot(data[self.pos[0], self.pos[1], self.pos[2]], label = label)
+                    self.plot_ax.plot(data[pos[0], pos[1], pos[2]], label = label)
                 else:
-                    self.plot_ax.plot(times, data[self.pos[0], self.pos[1], self.pos[2]], label = label)
+                    self.plot_ax.plot(times, data[pos[0], pos[1], pos[2]], label = label)
             else:
                 raise ValueError("Incorrect dimensions")
-        self.plot_ax.legend(loc = "lower left", title = f"val: {self.img[self.pos[0],self.pos[1],self.pos[2]]:.2f}")
+        self.plot_ax.legend(loc = "lower left", title = f"pos: ({pos[0]},{pos[1]},{pos[1]}), val: {self.img[self.pos[0],self.pos[1],self.pos[2]]:.2f}")
         # self.plot_ax.set_aspect('equal')
 
         self.setAxis()
@@ -103,7 +110,7 @@ class IMGShow:
     
     def buttonClick(self,event):
         try:
-            x, y = int(event.xdata), int(event.ydata)
+            x, y = int(round(event.xdata,0)), int(round(event.ydata, 0))
             if event.inaxes == self.axial_ax:
                 self.pos[0] = x
                 self.pos[1] = y
@@ -131,7 +138,7 @@ def maxMinNorn(sig):
 
 norm_funcs = {"stand" : stand, "maxMinNorm" : maxMinNorn}
 
-def showCVRAnalysisResult(analysis_file : str, img_desc = 'cvrAmplitude', norm : str = "maxMinNorm", data_include = "bold+regressor+predictions", **custom_settings):
+def showCVRAnalysisResult(analysis_file : str, img_desc = 'cvrAmplitude', img_transform = None, norm : str = "maxMinNorm", data_include = "bold+regressor+predictions", apply_transform = True, **custom_settings):
     # settings
     settings = {"cmap" : "RdYlBu_r", "vcenter" : 0, "vmin" : -1, "vmax" : 1, "aspect" : "equal", "origin" : "lower", 'interpolation' : 'antialiased'}
     settings.update(custom_settings)
@@ -142,12 +149,41 @@ def showCVRAnalysisResult(analysis_file : str, img_desc = 'cvrAmplitude', norm :
     preamble = analys_file.split("_desc-analys_info")[0]
     # cvr file
     cvr_img = image.load_img(os.path.join(folder, preamble + f"_desc-{img_desc}_map.nii.gz"))
+    # apply_transform 
+    if apply_transform:
+        # store all corner points
+        points = []
+        for x,y,z in product(*[[0,s-1] for s in cvr_img.shape]):
+            points.append((x,y,z,1))
+        points = np.array(points).T
+        # transform using affine transform
+        affine_points = np.linalg.matmul(cvr_img.affine, points)
+        # find max and min
+        min_loc = np.min(affine_points, axis = 1)[:3] 
+        max_loc = np.max(affine_points, axis = 1)[:3]
+        # find the voxel with smallest scale
+        scale = np.abs(cvr_img.affine.diagonal()[:3]).min()
+        # create new affine matrix
+        affine = np.eye(4)
+        affine[[0,1,2], [0,1,2]] = scale
+        # set least value to min location
+        affine[:3,3] = min_loc
+        # make sure out shape cover whole field of view
+        out_shape = np.ceil((max_loc - min_loc) / scale).astype(int)
+        # create data transform matrix
+        data_transform = np.linalg.matmul(np.linalg.inv(cvr_img.affine), affine)
+        # resample cvr img
+        cvr_img = image.resample_img(cvr_img, affine, out_shape, force_resample=True)
+    else:
+        data_transform = np.eye(4)
     # cvr data
     cvr_data = cvr_img.get_fdata()
     # mask
-    cvr_mask = np.abs(cvr_data) < 1e-10
+    # cvr_mask = np.abs(cvr_data) < 1e-10
     # mask data
     cvr_img_masked = cvr_data #  np.ma.masked_where(cvr_mask, cvr_data)
+    if img_transform is not None:
+        cvr_img_masked = img_transform(cvr_img_masked)
     # data
     data = []
     # spit data include
@@ -158,7 +194,7 @@ def showCVRAnalysisResult(analysis_file : str, img_desc = 'cvrAmplitude', norm :
             tshift_img = image.load_img(os.path.join(folder, preamble + "_desc-cvrTimeshift_map.nii.gz"))
             data.append((tshift_img.get_fdata(), "timeshift"))
         except:
-            print("No bold img found")
+            print("No timeshift img found")
     # get bold data
     if "bold" in data_include:
         try:
@@ -187,22 +223,22 @@ def showCVRAnalysisResult(analysis_file : str, img_desc = 'cvrAmplitude', norm :
             with open(os.path.join(folder, preamble + "_desc-data_info.json"), "r") as file:
                 data_info = json.load(file)
             times = np.arange(0, correlations_img.shape[3]) * data_info["align-regressor-time-step"] + data_info["align-regressor-start-time"]
-	        
-            data.append(((times, norm_func(correlations_img.get_fdata())), "x-correlation"))
+            corr = correlations_img.get_fdata()
+            data.append(((times, norm_func(corr)), "x-correlation"))
         except:
             print("No correlation img found")
     # get voxel mask
     try:
         with open(os.path.join(folder, preamble + "_desc-data_info.json"), "r") as file:
             data_info = json.load(file)
-        voxel_mask = image.load_img(data_info['voxel-mask-file']).get_fdata()
-        voxel_mask = np.ma.masked_where(voxel_mask, voxel_mask)
+        voxel_mask_img = image.resample_to_img(data_info['voxel-mask-file'], cvr_img, force_resample=True, interpolation="nearest")
+        voxel_mask = np.ma.masked_where(voxel_mask_img.get_fdata(), voxel_mask_img.get_fdata())
         # voxel_mask = None
     except:
         voxel_mask = None
         print("Could not load voxel mask")
 
-    img_show = IMGShow(cvr_img_masked, voxel_mask, settings, 
+    img_show = IMGShow(cvr_img_masked, voxel_mask, settings, data_transform, 
                         *data)
     
     img_show.fig.suptitle(preamble)
