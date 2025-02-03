f743c30be18587af5023af44e8b45310bc52b3ee
gurraburra <gustav.magnusson@liu.se>
2025-02-03 10:46:47 +0100

    Made get_output_files more generic

--- src/cvr_analysis/legacy/get_output_files.py
+++ src/cvr_analysis/legacy/get_output_files.py
@@ -7,11 +7,12 @@ import pandas as pd
 def getOutputFiles(path):
     analysis_list = []
 
-    optional_fields = {field : None for field in ["analysis", "refinement", "task", "run"]}
+    optional_fields = {field : None for field in ["ses", "task", "run", "space", "analys", "refinement"]}
+    field_full_name = {"ses" : "session", "analys" : "analysis"}
 
-    def checkOptionalField(field, regex, file, row):
+    def checkOptionalField(field, file, row):
         try:
-            row.append(re.search(regex, file).group(1))
+            row.append(re.search(f"_{field}-([^_]*)_", file).group(1))
             if optional_fields[field] == False:
                 raise RuntimeError(f"Inconistent field: {field}")
             optional_fields[field] = True
@@ -24,27 +25,20 @@ def getOutputFiles(path):
     for file in glob(path):
         row = []
         row.append(re.search("/sub-(..)_", file).group(1))
-        row.append(re.search("_ses-([^_]*)_", file).group(1))
-        checkOptionalField("task", "_task-([^_]*)_", file, row)
-        checkOptionalField("run", "_run-(..)_", file, row)
-        row.append(re.search("_space-([^_]*)_", file).group(1))
-        checkOptionalField("analysis", "_analys-([^_]*)_", file, row)
-        checkOptionalField("refinement", "_refinement-([^_]*)_", file, row)
+        for field in optional_fields.keys():
+            checkOptionalField(field, file, row)
         row.append(file)
         row.append(datetime.fromtimestamp(os.path.getctime(file)))
         analysis_list.append(row)
     
     # columns
-    columns=["subject", "session"]
-    if optional_fields["task"]:
-        columns.append("task")
-    if optional_fields["run"]:
-        columns.append("run")
-    columns.append("space")
-    if optional_fields["analysis"]:
-        columns.append("analysis")
-    if optional_fields["refinement"]:
-        columns.append("refinement")
+    columns=["subject"]
+    for k,v in optional_fields.items():
+        if v:
+            if k in field_full_name:
+                columns.append(field_full_name[k])
+            else:
+                columns.append(k)
     columns.extend(["file", "date"])
 
     return pd.DataFrame(analysis_list, columns=columns)
