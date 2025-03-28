714f0b8378838cf167d846c0393e29d275630717
gurraburra <gustav.magnusson@liu.se>
2025-03-28 11:15:44 +0100

    Minor bug

--- src/cvr_analysis/workflows/doppler/__init__.py
+++ src/cvr_analysis/workflows/doppler/__init__.py
@@ -1,6 +1,6 @@
-from cvr_analysis.workflows.mr.cvr_wf import cvr_wf
-from cvr_analysis.workflows.mr.post_processing import data_loader_wf, signal_processing_wf
-from cvr_analysis.workflows.mr.regression import setup_regression_wf, iterate_cvr_wf
+from cvr_analysis.workflows.doppler.cvr_wf import cvr_wf
+from cvr_analysis.workflows.doppler.post_processing import data_loader_wf, signal_processing_wf
+from cvr_analysis.workflows.doppler.regression import setup_regression_wf, iterate_cvr_wf
 
 data_loader_wf.cacheNodesData(True)
 signal_processing_wf.cacheNodesData(True)
